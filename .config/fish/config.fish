872eff74258fab7420cc88d3f5cf2564772afb5c
Spenser Black <spenserblack01@gmail.com>
2022-02-25 20:02:51 -0500

    Add GPG and SSH environment variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,14 @@ if status is-interactive
     alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 end
 
+set -x GPG_TTY (tty)
+eval (ssh-agent -c)
+if not pgrep --full ssh-agent | string collect > /dev/null
+    eval (ssh-agent -c)
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
+
 set -x COWPATH "/usr/share/cowsay/cows:$HOME/cowfiles"
 set -x DENO_INSTALL $HOME/.deno
 set -x EDITOR nvim
