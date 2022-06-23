fbe269999bba5b3b97b9bb372a8f53f6240f5b91
Spenser Black <spenserblack01@gmail.com>
2022-06-23 11:20:02 -0400

    Add `google` script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
     alias nv=nvim
     alias mime="file --mime"
     alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+    alias ggl=google
 end
 
 set -x GPG_TTY (tty)
@@ -10,3 +11,4 @@ set -x GPG_TTY (tty)
 set -x COWPATH "/usr/share/cowsay/cows:$HOME/cowfiles"
 set -x DENO_INSTALL $HOME/.deno
 set -x EDITOR nvim
+set -x BROWSER chromium-browser
