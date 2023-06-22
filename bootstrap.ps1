783a5028d7b32c476e0fdd2aa1fc7ae4ae9104c9
Spenser Black <spenserblack01@gmail.com>
2023-06-22 15:20:24 -0400

    Set PowerShell bell style

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,6 +2,7 @@
 New-Item -Force -Path $PROFILE
 echo @"
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineOption -BellStyle Visual
 Function Django {python.exe manage.py @Args}
 Set-Alias dj -Value Django
 Function Beep {[console]::beep(256,128)}
