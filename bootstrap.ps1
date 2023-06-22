e287c1785715f9c242b835e7a1b4f6e691a38b22
Spenser Black <spenserblack01@gmail.com>
2023-06-22 14:57:01 -0400

    Add Django alias to PowerShell bootstrap

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,4 +2,6 @@
 New-Item -Force -Path $PROFILE
 echo @"
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Function Django {python.exe manage.py @Args}
+Set-Alias dj -Value Django
 "@ | Out-File $PROFILE
