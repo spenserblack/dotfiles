9ecc6d9a8b967a02b6bdc85941bef087be09c6e8
Spenser Black <spenserblack01@gmail.com>
2023-06-22 15:11:32 -0400

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -4,4 +4,5 @@ echo @"
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Function Django {python.exe manage.py @Args}
 Set-Alias dj -Value Django
+Function Beep {[console]::beep(256,128)}
 "@ | Out-File $PROFILE
