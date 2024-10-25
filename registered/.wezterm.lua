58a2906212163f735779862b7769fcd74861dc58
Spenser Black <spenserblack01@gmail.com>
2024-10-24 20:12:15 -0400

    Style wezterm window using Rosé Pine palette

--- registered/.wezterm.lua
+++ registered/.wezterm.lua
@@ -12,9 +12,38 @@ local next_theme = {
   [dark_theme] = light_theme,
   [light_theme] = dark_theme,
 }
+-- Window colors from https://rosepinetheme.com/palette/
+local dark_active_window_bg = "#232136"
+local light_active_window_bg = "#faf4ed"
+local appearance_active_window_bg = {
+  Light = light_active_window_bg,
+  Dark = dark_active_window_bg,
+}
+local next_active_window_bg = {
+  [dark_active_window_bg] = light_window_bg,
+  [light_active_window_bg] = dark_window_bg,
+}
+local dark_active_window_fg = "#e0def4"
+local light_active_window_fg = "#575279"
+local appearance_active_window_fg = {
+  Light = light_active_window_fg,
+  Dark = dark_active_window_fg,
+}
+local next_active_window_fg = {
+  [dark_active_window_fg] = light_active_window_fg,
+  [light_active_window_fg] = dark_active_window_fg,
+}
 
 local appearance = wezterm.gui.get_appearance()
 config.color_scheme = appearance_themes[appearance] or dark_theme
+config.window_frame = {
+  active_titlebar_bg = appearance_active_window_bg[appearance] or dark_active_window_bg,
+  active_titlebar_fg = appearance_active_window_fg[appearance] or dark_active_window_fg,
+  inactive_titlebar_bg = appearance_active_window_bg[appearance] or dark_active_window_bg,
+  inactive_titlebar_fg = appearance_active_window_fg[appearance] or dark_active_window_fg,
+  button_bg = appearance_active_window_bg[appearance] or dark_active_window_bg,
+  button_fg = appearance_active_window_fg[appearance] or dark_active_window_fg,
+}
 
 config.window_background_opacity = 0.95
 
