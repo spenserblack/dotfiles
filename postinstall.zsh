58eb35cfdf53182c9077b714ea9f79ba373fbf42
Spenser Black <spenserblack01@gmail.com>
2024-10-29 19:30:52 -0400

    Initialize PNPM completions if PNPM is installed

--- postinstall.zsh
+++ postinstall.zsh
@@ -5,3 +5,7 @@ mkdir -p "$HOME/.zsh_completions"
 if [[ "$(which gh)" != "" ]]; then
   gh completion -s zsh > "$HOME/.zsh_completions/_gh.zsh"
 fi
+
+if [[ "$(which pnpm)" != "" ]]; then
+  pnpm completion zsh > "$HOME/.zsh_completions/_pnpm.zsh"
+fi
