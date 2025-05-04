9d882bb575ec8338395b34b1ba3b4018c6a5883b
Spenser Black <spenserblack01@gmail.com>
2025-05-04 11:52:55 -0400

    Add `.nvim` plugin

--- .linux/.config/nvim/lua/plugins.lua
+++ .linux/.config/nvim/lua/plugins.lua
@@ -2,4 +2,5 @@ return {
   { "github/copilot.vim" },
   { "rose-pine/neovim", name = "rose-pine" },
   { "spenserblack/colorscheme.nvim", version = "3" },
+  { "spenserblack/dot-nvim.nvim", version = "0.1" },
 }
