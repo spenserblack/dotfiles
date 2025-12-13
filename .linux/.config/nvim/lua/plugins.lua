5d16d98720dbc06678e031e416caf3fcc2ce1847
Spenser Black <spenserblack01@gmail.com>
2025-12-13 11:43:31 -0500

    Disable Copilot in NeoVim

--- .linux/.config/nvim/lua/plugins.lua
+++ .linux/.config/nvim/lua/plugins.lua
@@ -1,5 +1,4 @@
 return {
-  { "github/copilot.vim" },
   { "rose-pine/neovim", name = "rose-pine" },
   { "spenserblack/colorscheme.nvim", version = "3" },
   { "spenserblack/dot-nvim.nvim", version = "0.1" },
